3bcb2fe5a7d35fd6a5060b28f2f4f05dfc5ba02e
Hussnain Cheema <76845698+hussnaincheema@users.noreply.github.com>
2023-09-25 00:57:28 -0400

    Screens Commited

---
+++ screens/AwarenessSingle.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+    View,
+    Pressable,
+    StyleSheet,
+    Modal,
+    Alert,
+    StatusBar,
+    SafeAreaView,
+    Text,
+    ScrollView,
+    FlatList,
+    Image
+} from 'react-native';
+import AwarenessSingle from '../components/AwarenessSingle';
+
+const AwarnesSingleScreen=()=>{
+    return (
+        <>
+            <AwarenessSingle/>
+        </>
+    )
+}
+
+export default AwarenessSingle;
